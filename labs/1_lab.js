df273f69ae55a79a57f98c949a022709bfb434be
Gabriel Martinez Rojas <anewlink@users.noreply.github.com>
2025-10-01 17:02:19 -0500

    Lab1. Progress Scroll with operators

---
+++ labs/1_lab.js
@@ -0,0 +1,23 @@
+// begin lesson code
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+//helpers
+function calculateScrollPercent(element) {
+  const { scrollTop, scrollHeight, clientHeight } = element;
+
+  return (scrollTop / (scrollHeight - clientHeight)) * 100;
+}
+
+// elems
+const progressBar = document.querySelector('.progress-bar');
+
+//streams
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+  // percentage progress
+  map(({ target }) => calculateScrollPercent(target.scrollingElement))
+);
+progress$.subscribe(percent => {
+    progressBar.style.width = `${percent}%`;
+});
